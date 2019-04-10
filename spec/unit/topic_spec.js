d507466252628e4a94dc45721c892364c4330a74
Derya O. Kurin <dozdemir663@gmail.com>
2019-04-10 19:34:29 -0400

    the-post-resource-part-2

---
+++ spec/unit/topic_spec.js
@@ -0,0 +1,71 @@
+const sequelize = require("../../src/db/models/index").sequelize;
+const Topic = require("../../src/db/models").Topic;
+const Post = require("../../src/db/models").Post;
+
+describe("Topic", () => {
+
+  beforeEach((done) => {
+
+    this.topic;
+    this.post;
+    sequelize.sync({force: true}).then((res) => {
+
+      Topic.create({
+        title: "Expeditions to Alpha Centauri",
+        description: "A compilation of reports from recent visits to the star system."
+      })
+      .then((topic) => {
+        this.topic = topic;
+
+        Post.create({
+          title: "My first visit to Proxima Centauri b",
+          body: "I saw some rocks.",
+          topicId: this.topic.id
+        })
+        .then((post) => {
+          this.post = post;
+          done();
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
+    });
+
+  });
+
+describe('#create()', () => {
+
+  it("should create a topic object with a title and a description", (done) => {
+
+    Topic.create({
+      title: "Favourite Novel",
+      description: "The Catcher in the Rye"
+    })
+    .then((topic) => {
+
+      expect(topic.title).toBe("Favourite Novel");
+      expect(topic.description).toBe("The Catcher in the Rye");
+      done();
+
+    })
+    .catch((err) => {
+      console.log(err);
+      done();
+    });
+  });
+ });
+
+ describe('#getPosts', () => {
+   it("should return the associated posts", (done) => {
+
+     this.topic.getPosts()
+     .then((associatedPosts) => {
+       expect(associatedPosts[0].title).toBe("My first visit to Proxima Centauri b");
+       done();
+     });
+  });
+});
+
+});
