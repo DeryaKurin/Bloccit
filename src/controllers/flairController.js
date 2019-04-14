e6e76c6aeb5c40343c0055a182cdff9a54674206
Derya O. Kurin <dozdemir663@gmail.com>
2019-04-13 20:08:24 -0400

    Implementation of flairs with CRUD operations3

--- src/controllers/flairController.js
+++ src/controllers/flairController.js
@@ -16,7 +16,7 @@ module.exports = {
       if(err) {
         res.redirect(500, "flairs/new");
       } else {
-        res.redirect(303, `posts/${newFlair.postId}/flairs/${flair.id}`);
+        res.redirect(303, `/topics/${req.params.topicId}/posts/${newFlair.postId}/flairs/${newFlair.id}`);
       }
     });
   },
@@ -34,9 +34,9 @@ module.exports = {
   destroy(req, res, next){
      flairQueries.deleteFlair(req.params.id, (err, deletedRecordsCount) => {
        if(err){
-         res.redirect(500, `/posts/${req.params.postId}/flairs/${req.params.id}`)
+         res.redirect(500, `/topics/${req.params.topicId}/posts/${req.params.postId}/flairs/${req.params.id}`)
        } else {
-         res.redirect(303, `/posts/${req.params.postId}`)
+         res.redirect(303, `/topics/${req.params.topicId}/posts/${req.params.postId}`)
        }
      });
    },
@@ -54,9 +54,9 @@ module.exports = {
    update(req, res, next){
      flairQueries.updateFlair(req.params.id, req.body, (err, flair) => {
        if(err || flair == null){
-         res.redirect(404, `/posts/${req.params.postId}/flairs/${req.params.id}/edit`);
+         res.redirect(404, `/topics/${req.params.topicId}/posts/${req.params.postId}/flairs/${req.params.id}/edit`);
        } else {
-         res.redirect(`/posts/${req.params.postId}/flairs/${req.params.id}`);
+         res.redirect(`/topics/${req.params.topicId}/posts/${req.params.postId}/flairs/${req.params.id}`);
        }
      });
    }
